a572413dc3233229c7d04830f87433889448c8cb
georgemayer <mayer.georgep@gmail.com>
2014-02-27 16:23:01 -0500

    progress on editing URLS

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -12,13 +12,15 @@ class Scraper
   def get_urls
     index_doc = Nokogiri::HTML(open(search_url))
     @blog_urls = index_doc.search("li.g a").collect{|e| e.attribute("href").value}
-    @blog_urls = blog_urls.slice(0,5)
+    @blog_urls = blog_urls.reject do |b|
+      /search\\?/.match(b)
+      end.slice(0,5).collect {|b| b.gsub("/url?q=","").split("&sa")[0]}
   end
 
   def get_splices
     blog_urls.collect do |blog_url|
       blog_doc = Nokogiri::HTML(open(blog_url))
-      blog = blog_doc.search("#content p:first").collect{|e| e.attribute("href").value}
+      blog = blog_doc.search("#content p:first").collect{|e| e.value}
     end
   end
 end

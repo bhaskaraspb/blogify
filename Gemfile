ef949c5947f097cbc71e65da4130f014abc15ebe
georgemayer <mayer.georgep@gmail.com>
2014-03-10 11:00:49 -0400

    gems

--- Gemfile
+++ Gemfile
@@ -10,6 +10,10 @@ group :development do
   gem 'sqlite3'
 end
 
+group :production do 
+  gem 'pg'
+end
+
 gem 'pry'
 gem 'rubypress'
 gem 'shotgun'
--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,28 @@ Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| requir
 
 require_relative '../app.rb'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/development.sqlite3"
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+ 
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
   )
+end
+
+configure :development do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+ 
+  ActiveRecord::Base.establish_connection(
+      :adapter => "sqlite3",
+      :database => "db/development.sqlite3"
+  )
+end
+
+
+
